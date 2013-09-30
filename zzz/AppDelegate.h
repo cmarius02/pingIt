2bb7d62d65543fb42827f3c585a16183a379ebc0
Marius Cotofana <cmarius02@gmail.com>
2013-09-30 23:29:18 +0300

    Initial Commit

---
+++ zzz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zzz
+//
+//  Created by Marius Coțofană on 9/30/13.
+//  Copyright (c) 2013 Marius Coțofană. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ zzz/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  zzz
+//
+//  Created by Marius Coțofană on 9/30/13.
+//  Copyright (c) 2013 Marius Coțofană. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
